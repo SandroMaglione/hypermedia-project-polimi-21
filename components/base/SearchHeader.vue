43dc55453c65f9e14db127249006049cde1e014b
Sandro Maglione <lass.maglio@gmail.com>
2021-06-08 18:08:35 +0200

    [FIX]: Search header layout

--- components/base/SearchHeader.vue
+++ components/base/SearchHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- Header with title, subtitle, and search input -->
   <div class="flex flex-col items-center space-y-6 lg:flex-row">
-    <div class="flex-1 w-full text-left">
+    <div class="flex-1 text-left">
       <base-section-title :title="title" :subtitle="subtitle" />
     </div>
-    <div class="flex-none w-full">
+    <div class="flex-none">
       <input
         id="search"
         type="text"
